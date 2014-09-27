75e3c98ad05603f64e387c9e57831d22fb281e10
jpadams <jeremy@puppetlabs.com>
2014-09-26 20:36:23 -0700

    Added forcelocal to users and groups

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,32 @@
 class remove_while_upgrading {
 
-  User<| title == 'peadmin' or title == 'puppet-dashboard' |> {
-    shell => '/sbin/nologin',
+  User <| title == 'pe-activemq'  or
+          title == 'pe-apache'    or
+          title == 'pe-auth'      or
+          title == 'pe-memcached' or
+          title == 'pe-postgres'  or
+          title == 'pe-puppet'    or
+          title == 'pe-puppetdb'  or
+          title == 'peadmin'      or
+          title == 'puppet-dashboard' |> {
+    forcelocal => true,
+  }
+
+  User <| title == 'peadmin'      or
+          title == 'puppet-dashboard' |> {
+      shell => '/sbin/nologin',
+  }
+
+  Group <| title == 'pe-activemq'  or
+           title == 'peadmin'      or
+           title == 'pe-apache'    or
+           title == 'pe-auth'      or
+           title == 'pe-memcached' or
+           title == 'pe-postgres'  or
+           title == 'pe-puppetdb'  or
+           title == 'pe-puppet'    or
+           title == 'puppet-dashboard' |> {
+    forcelocal => true,
   }
 
 }
