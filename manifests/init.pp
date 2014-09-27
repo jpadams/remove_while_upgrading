aa3ea4e0438feaad9b16b340e541baf42c13fb79
jpadams <jeremy@puppetlabs.com>
2014-09-26 21:08:29 -0700

    reorder

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,13 @@
 class remove_while_upgrading {
 
   Group <| title == 'pe-activemq'  or
-           title == 'peadmin'      or
            title == 'pe-apache'    or
            title == 'pe-auth'      or
            title == 'pe-memcached' or
            title == 'pe-postgres'  or
-           title == 'pe-puppetdb'  or
            title == 'pe-puppet'    or
+           title == 'pe-puppetdb'  or
+           title == 'peadmin'      or
            title == 'puppet-dashboard' |> {
     forcelocal => true,
   }
